d6b37989cfb428acd749f214c3d080e30514afc4
Leonardo Perez <A01665462@tec.mx>
2026-04-17 17:22:30 -0600

    feat: BE-129-backend Define and implement get by provider id method on user repository

--- src/main/java/com/itesm/domain/repository/UserRepository.java
+++ src/main/java/com/itesm/domain/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.itesm.domain.repository;
 
 import com.itesm.domain.models.User;
+import java.util.Optional;
 
 /**
  * UserRepository
  */
 public interface UserRepository {
     User save(User user);
+    Optional<User> findByProviderUid(String providerUid);
 }
--- src/main/java/com/itesm/infrastructure/persistence/repository/UserRepositoryImpl.java
+++ src/main/java/com/itesm/infrastructure/persistence/repository/UserRepositoryImpl.java
@@ -6,6 +6,7 @@ import com.itesm.infrastructure.mapper.UserMapper;
 import com.itesm.infrastructure.persistence.entity.UserEntity;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.transaction.Transactional;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -18,5 +19,12 @@ public class UserRepositoryImpl implements UserRepository, PanacheRepositoryBase
         UserEntity entity = UserMapper.toEntity(user);
         persist(entity);
         return UserMapper.toDomain(entity);
+    }
+
+    @Override
+    @Transactional
+    public Optional<User> findByProviderUid(String providerUid) {
+        UserEntity entity = find("providerUid", providerUid).firstResult();
+        return Optional.ofNullable(entity).map(UserMapper::toDomain);
     }
 }
