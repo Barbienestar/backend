6ba0ea2ff6bdd5909c25b7845c5b90bd0b30517b
Leonardo Perez <A01665462@tec.mx>
2026-04-17 17:04:07 -0600

    feat: BE-129-backend create user model

---
+++ src/main/java/com/itesm/domain/models/User.java
@@ -0,0 +1,17 @@
+package com.itesm.domain.models;
+
+import java.util.UUID;
+
+/**
+ * User
+ */
+public class User {
+    private UUID id;
+    private String name;
+    private String lastName1;
+    private String lastName2;
+    private String email;
+    private String providerUid;
+    private boolean active;
+    private String role;
+}
