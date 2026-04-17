3fe4b29da7cc0f178f7df123141daf1baa8ef014
Leonardo Perez <A01665462@tec.mx>
2026-04-17 17:19:44 -0600

    feat: BE-129-backend Complete repository implementation for users

--- src/main/java/com/itesm/infrastructure/persistence/repository/UserRepositoryImpl.java
+++ src/main/java/com/itesm/infrastructure/persistence/repository/UserRepositoryImpl.java
@@ -2,6 +2,8 @@ package com.itesm.infrastructure.persistence.repository;
 
 import com.itesm.domain.models.User;
 import com.itesm.domain.repository.UserRepository;
+import com.itesm.infrastructure.mapper.UserMapper;
+import com.itesm.infrastructure.persistence.entity.UserEntity;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.transaction.Transactional;
 import java.util.UUID;
@@ -9,9 +11,12 @@ import java.util.UUID;
 /**
  * UserRepositoryImpl
  */
-public class UserRepositoryImpl implements UserRepository, PanacheRepositoryBase<User, UUID> {
+public class UserRepositoryImpl implements UserRepository, PanacheRepositoryBase<UserEntity, UUID> {
     @Override
     @Transactional
     public User save(User user) {
+        UserEntity entity = UserMapper.toEntity(user);
+        persist(entity);
+        return UserMapper.toDomain(entity);
     }
 }
