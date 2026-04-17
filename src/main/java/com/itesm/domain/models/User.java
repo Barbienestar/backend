da098cb68d3aaaf895a572020908fc73ae648dcd
Leonardo Perez <A01665462@tec.mx>
2026-04-17 17:13:48 -0600

    feat: BE-129-backend Create constructors, getters and setters to domain user model

--- src/main/java/com/itesm/domain/models/User.java
+++ src/main/java/com/itesm/domain/models/User.java
@@ -14,4 +14,82 @@ public class User {
     private String providerUid;
     private boolean active;
     private String role;
+
+    public User() {
+    }
+
+    public User(UUID id, String name, String lastName1, String lastName2, String email,
+        String providerUid, boolean active, String role) {
+        this.name = name;
+        this.lastName1 = lastName1;
+        this.lastName2 = lastName2;
+        this.email = email;
+        this.providerUid = providerUid;
+        this.active = active;
+        this.role = role;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName1() {
+        return lastName1;
+    }
+
+    public void setLastName1(String lastName1) {
+        this.lastName1 = lastName1;
+    }
+
+    public String getLastName2() {
+        return lastName2;
+    }
+
+    public void setLastName2(String lastName2) {
+        this.lastName2 = lastName2;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProviderUid() {
+        return providerUid;
+    }
+
+    public void setProviderUid(String providerUid) {
+        this.providerUid = providerUid;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
