a822f0165225f2e5b93c9c764be93bb8e102494a
Leonardo Perez <A01665462@tec.mx>
2026-04-24 09:18:21 -0600

    feat: Create security application interfaces

---
+++ src/main/java/com/itesm/application/security/CurrentUser.java
@@ -0,0 +1,46 @@
+package com.itesm.application.security;
+
+import java.util.UUID;
+
+/**
+ * CurrentUser
+ */
+public class CurrentUser {
+    private final UUID id;
+    private final String firebaseUid;
+    private final String email;
+    private final String role;
+    private final String name;
+
+    public CurrentUser(UUID id, String firebaseUid, String email, String role, String name) {
+        this.id = id;
+        this.firebaseUid = firebaseUid;
+        this.email = email;
+        this.role = role;
+        this.name = name;
+    }
+
+    public Boolean hasRole(String role) {
+        return this.role.equals(role);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getFirebaseUid() {
+        return firebaseUid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
