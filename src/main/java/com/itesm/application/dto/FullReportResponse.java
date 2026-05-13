b0df48dcadfac1f32bb8d2b65a92b99c66f578a5
Leonardo Perez <A01665462@tec.mx>
2026-05-13 00:17:16 -0600

    feat: use json_property to return snake-case objects

--- src/main/java/com/itesm/application/dto/FullReportResponse.java
+++ src/main/java/com/itesm/application/dto/FullReportResponse.java
@@ -1,5 +1,7 @@
 package com.itesm.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,10 +10,22 @@ import lombok.Data;
 @AllArgsConstructor
 public class FullReportResponse {
     private String description;
+
+    @JsonProperty("image_url")
     private String imageUrl;
+
+    @JsonProperty("user_full_name")
     private String userFullName;
+
+    @JsonProperty("medicine_name")
     private String medicineName;
+
+    @JsonProperty("medicine_presentation")
     private String medicinePresentation;
+
+    @JsonProperty("medicine_dosage_form")
     private String medicineDosageForm;
+
+    @JsonProperty("hospital_name")
     private String hospitalName;
 }
