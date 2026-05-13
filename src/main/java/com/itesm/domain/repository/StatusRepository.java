d50762c686d0c412e95c957af1b68a7775a04872
Leonardo Palatto <A01665462@tec.mx>
2026-05-13 17:47:33 -0600

    feat: Create list status endpoint (#52)

--- src/main/java/com/itesm/domain/repository/StatusRepository.java
+++ src/main/java/com/itesm/domain/repository/StatusRepository.java
@@ -1,11 +1,13 @@
 package com.itesm.domain.repository;
 
 import com.itesm.domain.models.Status;
-import com.itesm.infrastructure.persistence.entity.StatusEntity;
 
 import java.util.List;
 
 public interface StatusRepository {
     Status findStatusByName(String name);
+
     Status findStatusById(Byte id);
+
+    List<Status> getAll();
 }
