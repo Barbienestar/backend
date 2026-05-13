5a259aea5ad9044066560e48cd950e25e3f3c2df
Leonardo Perez <A01665462@tec.mx>
2026-05-13 14:46:52 -0600

    feat: Map reports to a signed firebase url

--- src/main/java/com/itesm/application/usecase/GetReportsByStatusUseCase.java
+++ src/main/java/com/itesm/application/usecase/GetReportsByStatusUseCase.java
@@ -3,22 +3,27 @@ package com.itesm.application.usecase;
 import com.itesm.application.dto.FullReportResponse;
 import com.itesm.application.dto.PagedResult;
 import com.itesm.domain.models.Report;
+import com.itesm.domain.repository.ImageRepository;
 import com.itesm.domain.repository.ReportRepository;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /** GetReportsByStatusUseCase */
 @ApplicationScoped
 public class GetReportsByStatusUseCase {
     private final ReportRepository reportRepository;
+    private final ImageRepository imageRepository;
 
     @Inject
-    public GetReportsByStatusUseCase(ReportRepository reportRepository) {
+    public GetReportsByStatusUseCase(
+            ReportRepository reportRepository, ImageRepository imageRepository) {
         this.reportRepository = reportRepository;
+        this.imageRepository = imageRepository;
     }
 
     public PagedResult<FullReportResponse> execute(
@@ -33,10 +38,13 @@ public class GetReportsByStatusUseCase {
     }
 
     private FullReportResponse toFullReportResponse(Report report) {
+
+        String signedUrl =
+                imageRepository.generateSignedUrl(report.getImageUrl(), 30, TimeUnit.MINUTES);
         FullReportResponse response =
                 new FullReportResponse(
                         report.getDescription(),
-                        report.getImageUrl(),
+                        signedUrl,
                         report.getUser().getName()
                                 + " "
                                 + report.getUser().getLastName1()
