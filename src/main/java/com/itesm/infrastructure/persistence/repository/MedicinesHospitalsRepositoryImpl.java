76fbdb7beb4ef088dc8497d25eed2a99e8d5aa57
Jorge Emiliano Cadena DIéguez <a01660252@tec.mx>
2026-05-19 21:11:33 -0600

    Fix/remove unused import (#73)
    * Removed unused import

--- src/main/java/com/itesm/infrastructure/persistence/repository/MedicinesHospitalsRepositoryImpl.java
+++ src/main/java/com/itesm/infrastructure/persistence/repository/MedicinesHospitalsRepositoryImpl.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import com.itesm.domain.models.MedicinesHospitals;
 import com.itesm.domain.models.MedicinesHospitalsStock;
