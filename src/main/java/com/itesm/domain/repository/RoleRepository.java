ebaf722134728ba283588c7a55bdad5bdd022bbe
Leonardo Perez <A01665462@tec.mx>
2026-05-22 08:40:37 -0600

    feat: add RoleRepository domain interface with mapper

---
+++ src/main/java/com/itesm/domain/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.itesm.domain.repository;
+
+import com.itesm.domain.models.Role;
+
+import java.util.Optional;
+
+public interface RoleRepository {
+    Optional<Role> findByName(String name);
+}
--- src/main/java/com/itesm/infrastructure/persistence/repository/RoleRepositoryImpl.java
+++ src/main/java/com/itesm/infrastructure/persistence/repository/RoleRepositoryImpl.java
@@ -1,11 +1,23 @@
 package com.itesm.infrastructure.persistence.repository;
 
+import com.itesm.domain.models.Role;
+import com.itesm.domain.repository.RoleRepository;
+import com.itesm.infrastructure.mapper.RoleMapper;
 import com.itesm.infrastructure.persistence.entity.RoleEntity;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
-@ApplicationScoped
-public class RoleRepositoryImpl implements PanacheRepositoryBase<RoleEntity, Byte> {}
+import java.util.Optional;
 
+@ApplicationScoped
+public class RoleRepositoryImpl implements RoleRepository, PanacheRepositoryBase<RoleEntity, Byte> {
+
+    @Override
+    public Optional<Role> findByName(String name) {
+        return find("name", name)
+                .firstResultOptional()
+                .map(RoleMapper::toDomain);
+    }
+}
