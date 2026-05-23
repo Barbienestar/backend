301b240441e4493f25545a6c051af44cbb7c7b9d
Leonardo Perez <A01665462@tec.mx>
2026-05-23 15:13:47 -0600

    feat: add ReportsSnapshotRepository interface

---
+++ src/main/java/com/itesm/domain/repository/ReportsSnapshotRepository.java
@@ -0,0 +1,10 @@
+package com.itesm.domain.repository;
+
+import com.itesm.application.dto.PeriodReportsByHospitalResponse;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ReportsSnapshotRepository {
+    List<PeriodReportsByHospitalResponse> getPeriodReports(
+            Integer idHospital, LocalDate startDate, LocalDate endDate);
+}
