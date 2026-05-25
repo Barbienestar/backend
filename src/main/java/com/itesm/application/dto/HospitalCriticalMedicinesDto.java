b759bdcdb9980f58db470825d6b7d32e641ff3df
Daniel Esteban <danyjudoka93@gmail.com>
2026-05-24 20:44:56 -0600

    Added @Schema annotations for open api documentation

--- src/main/java/com/itesm/application/dto/HospitalCriticalMedicinesDto.java
+++ src/main/java/com/itesm/application/dto/HospitalCriticalMedicinesDto.java
@@ -5,17 +5,23 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Hospital with at least one medicine whose stock is at or below the critical threshold")
 public class HospitalCriticalMedicinesDto {
+
     @JsonProperty("hospital_id")
+    @Schema(description = "Hospital identifier")
     private Integer hospitalId;
 
     @JsonProperty("hospital_name")
+    @Schema(description = "Name of the hospital")
     private String hospitalName;
 
     @JsonProperty("critical_medicines")
+    @Schema(description = "Medicines with critical stock levels, ordered by stock ASC")
     private List<CriticalMedicineDto> criticalMedicines;
 }
