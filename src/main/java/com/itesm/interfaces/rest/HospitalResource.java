f77fade6396a2ea5f7a09f79a3a3c7d216d45cbc
Daniel Esteban <danyjudoka93@gmail.com>
2026-05-24 19:56:21 -0600

    Added the hospitals/critical-medicines endpoint

--- src/main/java/com/itesm/interfaces/rest/HospitalResource.java
+++ src/main/java/com/itesm/interfaces/rest/HospitalResource.java
@@ -1,7 +1,10 @@
 package com.itesm.interfaces.rest;
 
+import com.itesm.application.dto.HospitalCriticalMedicinesDto;
 import com.itesm.application.dto.HospitalDto;
 import com.itesm.application.security.PermitPublic;
+import com.itesm.application.security.RequireRoles;
+import com.itesm.application.usecase.GetCriticalMedicinesUseCase;
 import com.itesm.application.usecase.GetHospitalsUseCase;
 import com.itesm.application.usecase.GetMyHospitalsUseCase;
 import jakarta.inject.Inject;
@@ -17,11 +20,16 @@ public class HospitalResource {
 
     private final GetHospitalsUseCase getHospitalsUseCase;
     private final GetMyHospitalsUseCase getMyHospitalsUseCase;
+    private final GetCriticalMedicinesUseCase getCriticalMedicinesUseCase;
 
     @Inject
-    public HospitalResource(GetHospitalsUseCase getHospitalsUseCase, GetMyHospitalsUseCase getMyHospitalsUseCase) {
+    public HospitalResource(
+            GetHospitalsUseCase getHospitalsUseCase,
+            GetMyHospitalsUseCase getMyHospitalsUseCase,
+            GetCriticalMedicinesUseCase getCriticalMedicinesUseCase) {
         this.getHospitalsUseCase = getHospitalsUseCase;
         this.getMyHospitalsUseCase = getMyHospitalsUseCase;
+        this.getCriticalMedicinesUseCase = getCriticalMedicinesUseCase;
     }
 
     @GET
@@ -36,5 +44,13 @@ public class HospitalResource {
     public Response getMyHospitals() {
         List<HospitalDto> hospitals = getMyHospitalsUseCase.execute();
         return Response.ok(hospitals).build();
+    }
+
+    @GET
+    @Path("/critical-medicines")
+    @RequireRoles({"health"})
+    public Response getCriticalMedicines() {
+        List<HospitalCriticalMedicinesDto> result = getCriticalMedicinesUseCase.execute();
+        return Response.ok(result).build();
     }
 }
