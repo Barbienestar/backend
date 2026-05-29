c41d84303468141b6f133c53866b322ae10a76f3
Leonardo Palatto <A01665462@tec.mx>
2026-05-29 09:39:34 -0600

    Fix/update user profile (#104)
    * fix: Fix casing to accept frontend casing
    
    * fix: adjust integraion tests

--- src/main/java/com/itesm/application/dto/UpdateUserDto.java
+++ src/main/java/com/itesm/application/dto/UpdateUserDto.java
@@ -7,10 +7,10 @@ import lombok.Data;
 public class UpdateUserDto {
     private String name;
 
-    @JsonProperty("last_name_1")
+    @JsonProperty("last_name1")
     private String lastName1;
 
-    @JsonProperty("last_name_2")
+    @JsonProperty("last_name2")
     private String lastName2;
 
     private Byte age;
